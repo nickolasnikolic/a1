26ad0b9dd54b4ce47aa3691002d9b65d0c282438
Nickolas Nikolic <subclass@gmail.com>
2015-10-15 13:36:18 -0500

    google maps errors up the wazoo/ structure

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,9 +25,10 @@ sApp.controller('LocationController', ['$scope', '$state', '$stateParams', 'uiGm
     $scope.location = _.findWhere($scope.locations, where);
 
     var address;
+    var passVal = $scope.location;
     uiGmapGoogleMapApi.then(function(maps) {
         var geocoder = new google.maps.Geocoder;
-        geocoder.geocode({address: $scope.location.address1}, function(result){
+        geocoder.geocode({address: passVal.address1 + passVal.zip }, function(result){
             console.log(result);
             //set map in place
             $scope.map = { center: { latitude: result[0].geometry.location.lat,  longitude: result[0].geometry.location.lng }, zoom: 8 };
