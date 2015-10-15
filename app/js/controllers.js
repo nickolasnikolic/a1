514fe227d3499906119cff883bc0f2cefe6165eb
Nickolas Nikolic <subclass@gmail.com>
2015-10-15 15:49:40 -0500

    google maps errors up the wazoo/ structure

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,8 +7,63 @@ sApp.controller('HomeController', ['$scope', '$state', 'globals', function($scop
     $scope.getDistance = function(){
         var zip = $('#zip').val();
         if(zip.length > 4){
-            //pull distance matrix from google maps
-            alert('running');
+            var map;
+            var latlng1;
+            var latlng2;
+
+            GMaps.geocode({
+                address: zip,
+                callback: function (results, status) {
+                    if (status == 'OK') {
+                        latlng1 = results[0].geometry.location;
+
+                        var locations = $scope.locations;
+
+                        _.each( $scope.locations, function( element, index, list ){
+
+                            var address = element.address1 + ' ' + element.zip;
+
+                            GMaps.geocode({
+                                address: address,
+                                callback: function (results, status) {
+                                    if (status == 'OK') {
+                                        latlng2 = results[0].geometry.location;
+                                        map = new GMaps({
+                                            div: '#map',
+                                            lat: latlng1.lat(),
+                                            lng: latlng1.lng(),
+                                            zoom: 12
+                                        });
+
+                                        map.drawRoute({
+                                            origin: [latlng1.lat(), latlng1.lng()],
+                                            destination: [latlng2.lat(), latlng2.lng()],
+                                            travelMode: 'driving',
+                                            strokeColor: '#131540',
+                                            strokeOpacity: 0.6,
+                                            strokeWeight: 6
+                                        });
+                                        map.getRoutes({
+                                            origin: [latlng1.lat(), latlng1.lng()],
+                                            destination: [latlng2.lat(), latlng2.lng()],
+                                            callback: function (e) {
+                                                var time = 0;
+                                                var distance = 0;
+                                                for (var i=0; i<e[0].legs.length; i++) {
+                                                    time += e[0].legs[i].duration.value;
+                                                    distance += e[0].legs[i].distance.value;
+                                                }
+                                                console.log(time + " " + distance);
+                                            }
+                                        });
+
+                                    }
+                                }
+                            });
+                        } )
+                    }
+                }
+            });
         }
     };
 
