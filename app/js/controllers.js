580edab1bf39ee33440e5fc6de593d07e3cf3654
Nickolas Nikolic <subclass@gmail.com>
2015-10-15 15:35:37 -0500

    google maps errors up the wazoo/ structure

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,10 +24,27 @@ sApp.controller('LocationController', ['$scope', '$state', '$stateParams', 'glob
 
     $scope.location = _.findWhere($scope.locations, where);
 
-    var map = new GMaps({
-        div: '#detailMap',
-        lat: -12.043333,
-        lng: -77.028333
+    var l = $scope.location;
+
+    GMaps.geocode({
+        address: l.address1 + ' ' + l.zip,
+        callback: function(results, status) {
+            if (status == 'OK') {
+                var latlng = results[0].geometry.location;
+
+                var map = new GMaps({
+                    div: '#detailMap',
+                    lat: -12.043333,
+                    lng: -77.028333
+                });
+
+                map.setCenter(latlng.lat(), latlng.lng());
+                map.addMarker({
+                    lat: latlng.lat(),
+                    lng: latlng.lng()
+                });
+            }
+        }
     });
 
 }])
