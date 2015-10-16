1e2e60dca8d154f11a33b9cd4fb8d81397337cee
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-10-16 12:45:27 -0500

    fleshing out data

--- app/js/services.js
+++ app/js/services.js
@@ -4,6 +4,7 @@ sApp.factory('globals', function() {
 
     Service.locations = [
         {
+            id: 1,
             name: 'Waukesha',
             phone: '1-262-547-8989',
             email: 'Alloneselfstorage@gmail.com',
@@ -33,7 +34,8 @@ sApp.factory('globals', function() {
                 '10x25', '11x25', '10x30',
                 '10x40', '20x20', '20x25', '20x30'
             ]
-        },        {
+        },{
+            id: 2,
             name: 'Pewaukee',
             phone: '1-262-547-8989',
             email: 'Alloneselfstorage@gmail.com',
@@ -62,7 +64,8 @@ sApp.factory('globals', function() {
                 '10x25', '11x25', '10x30',
                 '20x20'
             ]
-        },        {
+        },{
+            id: 3,
             name: 'Pewaukee',
             phone: '1-262-547-8989',
             email: 'Alloneselfstorage@gmail.com',
@@ -93,6 +96,7 @@ sApp.factory('globals', function() {
                 '10x40', '20x20', '20x25', '20x30'
             ]
         },{
+            id: 4,
             name: 'Muskego',
             phone: '1-262-547-8989',
             email: 'Alloneselfstorage@gmail.com',
@@ -121,6 +125,165 @@ sApp.factory('globals', function() {
                 '10x15', '10x20',
                 '10x22', '10x30',
                 '10x40', '30x40'
+            ]
+        },{
+            id: 5,
+            name: 'Badger Storage',
+            phone: '1-262-597-5444',
+            email: 'Sturtevantstorage@gmail.com',
+            fax: '1-262-458-4286',
+            after_hours_phone: '1-262-995-4884',
+            address1: '1587 22nd Avenue',
+            address2: '',
+            city: 'Kenosha',
+            state: 'WI',
+            zip: '53140',
+            images:[],
+            office: {
+                hours:{
+                    monday: '2pm-5pm',
+                    tuesday: '10am-1pm',
+                    wednesday: '10am-1pm',
+                    thursday: '10am-1pm',
+                    friday: '10am-1pm',
+                    saturday: '10am-1pm',
+                    sunday: 'by appointment'
+                }
+            },
+            driving_tip: '',
+            sizes:[
+                '5x5', '5x10', '10x10',
+                '10x15', '10x20',
+                '10x25', '10x30',
+                '10x40'
+            ]
+        },{
+            id: 6,
+            name: 'All One Self Storage LLC',
+            phone: '1-262-547-8989',
+            email: 'Alloneselfstorage@gmail.com',
+            fax: '1-262-970-8989',
+            after_hours_phone: '1-262-995-4884',
+            address1: '125 N. Oakridge Dr.',
+            address2: '',
+            city: 'North Prairie',
+            state: 'WI',
+            zip: '53153',
+            images:[],
+            office: {
+                hours:{
+                    monday: 'by appointment',
+                    tuesday: 'by appointment',
+                    wednesday: 'by appointment',
+                    thursday: 'by appointment',
+                    friday: 'by appointment',
+                    saturday: 'by appointment',
+                    sunday: 'by appointment'
+                }
+            },
+            driving_tip: 'Corner of Hwy 59 and Oakridge Dr. Next to the Sheriff\'s Dept',
+            amenities: 'Climate Controlled units available in 5x4 5x10 10x10 10x15 only',
+            sizes:[
+                '5x4', '5x10', '10x10',
+                '10x15', '10x20',
+                '10x25','10x27', '10x30',
+                '25x30'
+            ]
+        },{
+            id: 7,
+            name: 'All One Self Storage LLC',
+            phone: '1-262-547-8989',
+            email: 'Alloneselfstorage@gmail.com',
+            fax: '1-262-970-8989',
+            after_hours_phone: '1-262-995-4884',
+            address1: '133 N. Oakridge Dr.',
+            address2: '',
+            city: 'North Prairie',
+            state: 'WI',
+            zip: '53153',
+            images:[],
+            office: {
+                hours:{
+                    monday: 'by appointment',
+                    tuesday: 'by appointment',
+                    wednesday: 'by appointment',
+                    thursday: 'by appointment',
+                    friday: 'by appointment',
+                    saturday: 'by appointment',
+                    sunday: 'by appointment'
+                }
+            },
+            driving_tip: 'Corner of Hwy 59 and Oakridge Dr. Next to the Sheriff\'s Dept',
+            amenities: 'Climate Controlled units available in 5x4 5x10 10x10 10x15 only',
+            sizes:[
+                '5x4', '5x10', '10x10',
+                '10x15', '10x20',
+                '10x25','10x27', '10x30',
+                '25x30'
+            ]
+        },{
+            id: 8,
+            name: 'All One Self Storage LLC',
+            phone: '1-262-547-8989',
+            email: 'Alloneselfstorage@gmail.com',
+            fax: '1-262-970-8989',
+            after_hours_phone: '1-262-352-8989',
+            address1: '320 Venture Dr.',
+            address2: '',
+            city: 'Dousman',
+            state: 'WI',
+            zip: '53118',
+            images:[],
+            office: {
+                hours:{
+                    monday: 'by appointment',
+                    tuesday: 'by appointment',
+                    wednesday: 'by appointment',
+                    thursday: 'by appointment',
+                    friday: 'by appointment',
+                    saturday: 'by appointment',
+                    sunday: 'by appointment'
+                }
+            },
+            driving_tip: 'Located just south of the intersection of Hwy 18 and Hwy 67',
+            amenities: '',
+            sizes:[
+                '5x10', '10x10',
+                '10x15', '10x20', '10x22',
+                '10x25','10x30', '10x40',
+                '24x50'
+            ]
+        },{
+            id: 9,
+            name: 'All One Self Storage LLC',
+            phone: '1-262-547-8989',
+            email: 'Alloneselfstorage@gmail.com',
+            fax: '1-262-970-8989',
+            after_hours_phone: '1-262-352-8989',
+            address1: '340 Venture Dr.',
+            address2: '',
+            city: 'Dousman',
+            state: 'WI',
+            zip: '53118',
+            images:[],
+            office: {
+                hours:{
+                    monday: 'by appointment',
+                    tuesday: 'by appointment',
+                    wednesday: 'by appointment',
+                    thursday: 'by appointment',
+                    friday: 'by appointment',
+                    saturday: 'by appointment',
+                    sunday: 'by appointment'
+                }
+            },
+            driving_tip: 'Located just south of the intersection of Hwy 18 and Hwy 67',
+            amenities: '',
+            sizes:[
+                '5x10', '10x10',
+                '10x15', '10x20', '10x22',
+                '10x25','10x30', '10x40',
+                '24x50'
             ]
         },
     ];
